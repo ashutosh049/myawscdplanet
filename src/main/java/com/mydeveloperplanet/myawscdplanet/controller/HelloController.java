f00b5e6c61d3b3ff154271a9df12aca73aa3e26a
ashutosh049 <ashutosh049@outlook.com>
2021-04-03 20:54:06 +0530

    Add dto and refactoring

--- src/main/java/com/mydeveloperplanet/myawscdplanet/controller/HelloController.java
+++ src/main/java/com/mydeveloperplanet/myawscdplanet/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mydeveloperplanet.myawscdplanet;
+package com.mydeveloperplanet.myawscdplanet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
